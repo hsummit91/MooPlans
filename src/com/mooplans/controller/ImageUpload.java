2ff3afb6bf723d75d5fb877ddc7f2e63663ed69b
Summit <sumit_hotwani@yahoo.com>
2016-02-27 16:19:13 -0500

    Updated few files

--- src/com/mooplans/controller/ImageUpload.java
+++ src/com/mooplans/controller/ImageUpload.java
@@ -12,6 +12,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
 
+import org.apache.tomcat.util.http.fileupload.FileUtils;
+
 import com.mooplans.model.User;
 
 import static com.mooplans.dao.LoginDAO.*;
@@ -40,28 +42,28 @@ public class ImageUpload extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String savePath = SAVE_DIR;
-		String url = null, msg = null, path = null;
+		String url = null, path = null;
 		// creates the save directory if it does not exists
 		File fileSaveDir = new File(savePath);
 		if (!fileSaveDir.exists()) {
 			fileSaveDir.mkdir();
 		}
-		
+
 		HttpSession se = request.getSession();
 		User user = (User) se.getAttribute("User=");
 		int id = user.getUser_id();
 		for (Part part : request.getParts()) {
 			String fileName = extractFileName(part);
-			part.write(savePath + File.separator + fileName);
-			path = "imgs" + File.separator + fileName;
+
+			File imageDir = new File(savePath + File.separator + id);
+			FileUtils.deleteDirectory(imageDir);
+			imageDir.mkdir();
+			part.write(imageDir + File.separator + fileName);
+			path = "imgs" + File.separator + id + File.separator + fileName;
 			addImage(path, id);
 		}
-		url = "/home.jsp";
-		msg = "Image uploaded successfully";
-		
-		request.setAttribute("msg", msg);
-		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
-		dispatcher.forward(request, response);
+		url = "/jsp/imageUpload.jsp";
+		response.sendRedirect(getServletContext().getContextPath()+url);
 	}
 
 	/**
