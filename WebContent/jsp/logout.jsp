e1083666c26f5351d7ef11b93e461066d86b3d60
fahadshaikh05 <fahad.spring2015@gmail.com>
2016-03-13 17:48:03 -0400

    Addeed logout, feeedback,about us

--- WebContent/jsp/logout.jsp
+++ WebContent/jsp/logout.jsp
@@ -1,12 +1,27 @@
+<%@ page import="java.io.*,java.util.*" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
+<title>Logged Out</title>
+<script type="text/javaScript">
+	function disableBackButton() {
+		window.history.forward();
+	}
+	setTimeout("disableBackButton()", 0);
+</script>
 </head>
 <body>
-	<h1>BBye!</h1>
+<center>
+<h1>Page Redirection</h1>
+</center>
+<%
+   // New location to be redirected
+   String site = new String("http://localhost:8080/MooPlans/");
+   response.setStatus(response.SC_MOVED_TEMPORARILY);
+   response.setHeader("Location", site); 
+%>
 </body>
 </html>
---
+++ src/com/mooplans/controller/Feedback.java
@@ -0,0 +1,75 @@
+package com.mooplans.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.mooplans.dao.FeedbackDAO;
+import com.mooplans.model.User;
+
+/**
+ * Servlet implementation class Feedback
+ */
+@WebServlet("/Feedback")
+public class Feedback extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Feedback() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String url = "/jsp/home.jsp";
+		HttpSession session=request.getSession();
+		
+		int userId = Integer.parseInt((String) session.getAttribute("userId"));
+		String firstname= request.getParameter("firstname");
+		String emailId= request.getParameter("email");
+		String phone=request.getParameter("phone");
+		String message=request.getParameter("message");
+		
+		    System.out.println("userID ="+userId);
+			System.out.println("firstname ="+firstname);
+			System.out.println("userName ="+emailId);
+			System.out.println("phone ="+phone);
+			System.out.println("message ="+message);
+
+			User userObject = new User(firstname, emailId, phone, message, userId);
+
+			int ID = FeedbackDAO.insertFeedback(userObject);
+
+			if (ID == 0) {
+				System.out.println("Registraion failed ID :"+ID);
+				request.setAttribute("errormsg", "Registration failed. Please try again.");
+			}
+			else if (ID < 0) {
+				System.out.println("Registraion failed ID :"+ID);
+				request.setAttribute("errormsg", "Email ID already exists.");
+			}else{
+				System.out.println("Registraion done success ID :"+ID);
+				url = "/jsp/success.jsp";
+			}
+		response.sendRedirect(getServletContext().getContextPath()+url);
+	}
+
+}
