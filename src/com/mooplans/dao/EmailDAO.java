a56bd0116a819f9be393e11ee95e350b81ea311c
fahadshaikh05 <fahad.spring2015@gmail.com>
2016-04-29 23:28:06 -0400

    email

--- src/com/mooplans/dao/EmailDAO.java
+++ src/com/mooplans/dao/EmailDAO.java
@@ -55,7 +55,7 @@ public class EmailDAO {
 				sb.append("The Moo Plans Team");
 
 				message.setContent("<img src = \"http://i68.tinypic.com/157dn46.png\"/>"+"<br><br>"+sb.toString(), "text/html" );
-				message.setSubject("Registration!");
+				message.setSubject("Moo Plans - Registration!");
 				break;
 
 
@@ -63,7 +63,7 @@ public class EmailDAO {
 				sb.append("You've successfully added "+points+" points to your account.<br>");
 				sb.append("You have total "+user.getUser_points()+" points.<br><br>Happy Eating!");
 				message.setContent("<img src = \"http://i65.tinypic.com/j9buo4.jpg\"/>"+"<br><br>"+sb.toString(), "text/html" );
-				message.setSubject("Points Added!");
+				message.setSubject("Moo Plans - Points Added!");
 				break;
 			}
 			message.setSentDate(new Date());
