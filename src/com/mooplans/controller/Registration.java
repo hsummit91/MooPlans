6bfd64ea4c41e403422daba930e7c52b52bebc3c
omkar1212 <omkar_k_12@yahoo.com>
2016-08-28 21:51:09 -0400

    register page commit

--- src/com/mooplans/controller/Registration.java
+++ src/com/mooplans/controller/Registration.java
@@ -46,7 +46,7 @@ public class Registration extends HttpServlet {
 		
 		PrintWriter out = response.getWriter();
 		
-		String url = "/index.html";
+		String url = "/index.jsp";
 		String pay = "";
 		String errorMsg = "";
 		Boolean isError = false;
@@ -130,7 +130,7 @@ public class Registration extends HttpServlet {
 				
 				// Set success message
 				System.out.println("Registraion done success ID :"+ID);
-				url = "/index.html";
+				url = "/index.jsp";
 				errorMsg="Registration_Successful";
 				isError=true;
 				//pay="0";
